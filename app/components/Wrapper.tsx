ab89624f7441afa7e8e9491c01a930b5caa41f1e
lhuilliermarc <lhuilliermarc@free.fr>
2026-03-01 19:42:34 +0100

    formulaire création projet

--- app/components/Wrapper.tsx
+++ app/components/Wrapper.tsx
@@ -4,12 +4,22 @@ type WrapperProps = {
 
 import React from 'react'
 import Navbar from './Navbar'
+import { ToastContainer } from 'react-toastify'
 
 const Wrapper = ({children} : WrapperProps) => {
   return (
     <div>
         <Navbar/>
         <div className='px-5 md:px-[10%] mt-8 mb-10'>
+        <ToastContainer
+          position='top-right'
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          pauseOnHover
+          draggable
+        />
             {children}
         </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,70 @@
+"use client"
+import { useState } from "react";
 import Wrapper from "./components/Wrapper";
+import { FolderGit2 } from "lucide-react";
+import { createProject } from "./actions";
+import { useUser } from "@clerk/nextjs";
+import { toast } from "react-toastify";
 
 export default function Home() {
+
+  const {user} = useUser()
+  const email = user?.primaryEmailAddress?.emailAddress as string
+  const [name, setName] = useState("")
+  const [description, setDescription] = useState("")
+
+  const handleSubmit = async () => {
+    try {
+      const modal = document.getElementById('my_modal_3') as HTMLDialogElement
+      const project = await createProject(name, description, email)
+      if(modal){
+        modal.close()
+      }
+      setName("")
+      setDescription("")
+      toast.success("Projet créé")
+    } catch (error){
+      console.error('Error creating project:', error);
+    }
+  }
+
   return (
     <Wrapper>
-      <button className="btn btn-accent">test</button>
+      <div>
+        {/* You can open the modal using document.getElementById('ID').showModal() method */}
+          <button className="btn" onClick={()=>(document.getElementById('my_modal_3') as HTMLDialogElement).showModal()}>open modal</button>
+          <dialog id="my_modal_3" className="modal">
+            <div className="modal-box">
+              <form method="dialog">
+                {/* if there is a button in form, it will close the modal */}
+                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+              </form>
+              <h3 className="font-bold text-lg">Nouveau Projet</h3>
+              <p className="py-4">Décrivez votre projet grâce à la description</p>
+              <div>
+                <input
+                  placeholder="Nom du projet"
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className="border border-base-300 input input-borderer w-full mb-4 placeholder:text-sm"
+                  required
+                />
+                <textarea 
+                  placeholder="Description"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                  className="mb-2 textarea textarea-bordered border border-base-300 w-full textarea-md placeholder::text-sm"
+                  required
+                >
+                </textarea>
+                <button className="btn btn-primary" onClick={handleSubmit}>
+                  Nouveau Projet <FolderGit2/>
+                </button>
+              </div>
+            </div>
+          </dialog>
+      </div>
     </Wrapper>
   );
 }

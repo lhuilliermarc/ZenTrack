7fcbd31263ae5d66cdbaf8c84363b697021403e4
lhuilliermarc <lhuilliermarc@free.fr>
2026-03-03 20:58:36 +0100

    affichage projet par utilisateurs

--- app/general-projects/page.tsx
+++ app/general-projects/page.tsx
@@ -1,19 +1,37 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Wrapper from '../components/Wrapper'
 import { SquarePlus } from 'lucide-react'
 import { toast } from 'react-toastify'
-import { addUserToProject } from '../actions'
+import { addUserToProject, getProjectsAssociationWithUser } from '../actions'
 import { useUser } from '@clerk/nextjs'
+import { Project } from '@/type'
+import ProjectComponent from '../components/ProjectComponent'
+import EmptyState from '../components/EmptyState'
 
 const page = () => {
-    const {user} = useUser()
+    const { user } = useUser()
     const email = user?.primaryEmailAddress?.emailAddress as string
     const [inviteCode, setInviteCode] = useState("")
+    const [associatedProjects, setAssociatedProjects] = useState<Project[]>([])
+
+    const fetchProjects = async (email: string) => {
+        try {
+            const associated = await getProjectsAssociationWithUser(email)
+            setAssociatedProjects(associated)
+        } catch (error) {
+            toast.error("Erreur lors du chargement des projets:")
+        }
+    }
+    useEffect(() => {
+        if (email) {
+            fetchProjects(email)
+        }
+    }, [email])
 
     const handleSubmit = async () => {
         try {
-            if(inviteCode !=""){
+            if (inviteCode != "") {
                 await addUserToProject(email, inviteCode)
                 toast.success('Vous pouvez maintenant collaboré sur ce projet')
             } else {
@@ -37,8 +55,27 @@ const page = () => {
                     />
                 </div>
                 <button className='btn btn-primary ml-4' onClick={handleSubmit}>
-                    Rejoindre <SquarePlus className='w-4'/>
+                    Rejoindre <SquarePlus className='w-4' />
                 </button>
+            </div>
+            <div>
+                {associatedProjects.length > 0 ? (
+                    <ul className="w-full grid md:grid-cols-3 gap-6">
+                        {associatedProjects.map((project) => (
+                            <li key={project.id}>
+                                <ProjectComponent project={project} admin={0} style={true}></ProjectComponent>
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                    <div>
+                        <EmptyState
+                            imageSrc="/empty-project.png"
+                            imageAlt="Picture of an empty project"
+                            message="Aucun projet associé"
+                        />
+                    </div>
+                )}
             </div>
         </Wrapper>
     )
